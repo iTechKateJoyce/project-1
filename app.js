5ba1828481b163b437b0ccf37e0cad39a81ef730
katejoyce <katejoyce156@googlemail.com>
2021-05-09 17:37:28 +0100

    bug fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let lives = 3  
-let points = 0 
-let playerPosition = 93 //relates to index of cell array where player starts on the board, will be used to keep track of the frog too
+let lives = 3
+let points = 0
+let playerPosition = 93 //relates to index of cell array where player starts on the board, will be used to keep track of the frog too
 let logPosition = null
-const grid  = document.querySelector('.grid')//relates to container div for whole grid html element with grid class 
-const width = 9 
-const height = 12 
-const cells  =  [] //a single array of dom elements
+const grid = document.querySelector('.grid')//relates to container div for whole grid html element with grid class 
+const width = 9
+const height = 12
+const cells = [] //a single array of dom elements
 const carsRight = [72, 75, 78]//indicies of car starting point
 const carsLeft = [62, 59, 56]//indicies of car starting point
 const logsRight = [42, 41, 40, 38, 37, 36, 24, 23, 22, 20, 19, 18]// bothlanes
@@ -15,15 +15,15 @@ let time = 90
 //Game overlay - press enter to play
 document.addEventListener('keyup', (event) => {
   const startKey = event.key
-  if(startKey === 'Enter'){
+  if (startKey === 'Enter') {
     document.getElementById("overlay").style.display = 'none'
     document.getElementById("text").style.display = 'none'
     document.getElementById("p").style.display = 'none'
-  
+
   }
 })
 
-function generateGrid (grid, gridStyling) {//generate the grid 
+function generateGrid(grid, gridStyling) {//generate the grid 
   for (let index = 0; index < width * height; index++) {
     // Generate each element
     const cell = document.createElement('div')
@@ -40,65 +40,65 @@ function generateGrid (grid, gridStyling) {//generate the grid
 
 }
 function gridStyling(index, cell) {//adds grid styling/classes to cells
-  if(index === 100){ //add timer class to cell
+  if (index === 100) { //add timer class to cell
     cell.classList.add('countdown')
   }
-  else if(index >= 54 && index <= 89){ //add road styling
+  else if (index >= 54 && index <= 89) { //add road styling
     cell.classList.add('road')
   }
-  else if(index <= 53 && index >= 45){
+  else if (index <= 53 && index >= 45) {
     cell.classList.add('sidewalk')
   }
-  else if(index <= 44 && index >= 18){//adds river styling to grid
+  else if (index <= 44 && index >= 18) {//adds river styling to grid
     cell.classList.add('river')
   }
-  else if(index === 11 || index === 13 || index === 15){//adds home cells, these give you points if you reach them
+  else if (index === 11 || index === 13 || index === 15) {//adds home cells, these give you points if you reach them
     cell.classList.add('home')
   }
-  else if(index === 9 || index === 10 || index === 12 ||index === 14 || index === 16 || index === 17){
+  else if (index === 9 || index === 10 || index === 12 || index === 14 || index === 16 || index === 17) {
     cell.classList.add('grass')
   }
-  else if(index === 8){  //add points class to cell and display starting points
+  else if (index === 8) {  //add points class to cell and display starting points
     cell.classList.add('points')
     const displayScore = document.querySelector('.points')//grabs the points cell element
     displayScore.innerHTML = `Score ${points}`
   }
-  else if(index <= 107 && index >= 90 || index >= 0 && index <= 8){  //add status bar
+  else if (index <= 107 && index >= 90 || index >= 0 && index <= 8) {  //add status bar
     cell.classList.add('statusBar')//get rid of home row extras 
   }
-  else if(index === 106){ //adds lives class to cell 
+  else if (index === 106) { //adds lives class to cell 
     cell.classList.add('lives')
   }
 }
 generateGrid(grid, gridStyling)
-displayTimer() 
+displayTimer()
 
 const heart = '&#128154;'
 const heartSpan = document.createElement('span')
 heartSpan.classList.add('displayHearts')
 document.getElementById(106).appendChild(heartSpan)
 
-function displayLives(lives){ //displays current player lives inside .lives div
-  
-  if(lives > 0){
-    heartSpan.innerHTML = (`${heart}`.repeat( `${lives}`))
+function displayLives(lives) { //displays current player lives inside .lives div
+
+  if (lives > 0) {
+    heartSpan.innerHTML = (`${heart}`.repeat(`${lives}`))
   }
-  else if(lives <= 0){
+  else if (lives <= 0) {
     heartSpan.innerHTML = ''
   }
 }
 
-function displayTimer () {//displays countdown timer
+function displayTimer() {//displays countdown timer
   const displayTimer = document.querySelector('.countdown')//introduce the timer cell to JS
   let time = 90//90 seconds start
   displayTimer.innerHTML = time
   let intervalId = 0
   intervalId = setInterval(() => {
-    if (time <= 0){
+    if (time <= 0) {
       clearInterval(intervalId)
       displayTimer.innerHTML = '0s'
     }
-    else{
+    else {
       displayTimer.innerHTML = (`${time--}s`)
     }
   }, 900)
@@ -112,58 +112,58 @@ displayLives(lives)
 
 document.addEventListener('keyup', (event) => {//listens for keyup on arrows to trigger moveFrog() which controls frog movement & boundary logic
   const key = event.key
-    if(key === 'ArrowUp' && !((playerPosition - width) < width) ){
-      cells[playerPosition].classList.remove('frog')
-      playerPosition -= width
-      cells[playerPosition].classList.add('frog')
-      points += 10
-    }
-    else if(key === 'ArrowDown' && !((playerPosition + width) + width >= width * height)){
-      cells[playerPosition].classList.remove('frog')
-      playerPosition += width
-      cells[playerPosition].classList.add('frog')
-    }
-    else if(key === 'ArrowRight'&& !(playerPosition % width === width - 1)) {
-      cells[playerPosition].classList.remove('frog')
-      playerPosition += 1
-      cells[playerPosition].classList.add('frog')
-    }
-    else if(key === 'ArrowLeft' && !(playerPosition % width === 0)) {
-      cells[playerPosition].classList.remove('frog')
-      playerPosition -= 1
-      cells[playerPosition].classList.add('frog')
-    }
+  if (key === 'ArrowUp' && !((playerPosition - width) < width)) {
+    cells[playerPosition].classList.remove('frog')
+    playerPosition -= width
+    cells[playerPosition].classList.add('frog')
+    points += 10
+  }
+  else if (key === 'ArrowDown' && !((playerPosition + width) + width >= width * height)) {
+    cells[playerPosition].classList.remove('frog')
+    playerPosition += width
+    cells[playerPosition].classList.add('frog')
+  }
+  else if (key === 'ArrowRight' && !(playerPosition % width === width - 1)) {
+    cells[playerPosition].classList.remove('frog')
+    playerPosition += 1
+    cells[playerPosition].classList.add('frog')
+  }
+  else if (key === 'ArrowLeft' && !(playerPosition % width === 0)) {
+    cells[playerPosition].classList.remove('frog')
+    playerPosition -= 1
+    cells[playerPosition].classList.add('frog')
+  }
 })
 
-function resetGame (){ 
+function resetGame() {
   //remove any frogs that reached home
-const homeCells = [11, 13, 15]
-homeCells.forEach((home, i) =>{
-  if(cells[home].classList.contains('frogReachedHome')){
-    cells[home].classList.remove('frogReachedHome')
-  }
-})
-//reset frog position 
-cells[playerPosition].classList.remove('frog')//remove frog
-playerPosition = 94
-cells[playerPosition].classList.add('frog')
-  }
+  const homeCells = [11, 13, 15]
+  homeCells.forEach((home, i) => {
+    if (cells[home].classList.contains('frogReachedHome')) {
+      cells[home].classList.remove('frogReachedHome')
+    }
+  })
+  //reset frog position 
+  cells[playerPosition].classList.remove('frog')//remove frog
+  playerPosition = 94
+  cells[playerPosition].classList.add('frog')
+}
 
 const result = document.createElement('span')
 result.classList.add('result')
 document.getElementById(102).appendChild(result)
 
-function win (){
-  if(points >= 3000){
+function win() {
+  if (points >= 3000) {
     result.innerHTML = 'Winner'
     //add time bonus = points plus timervalue
 
     resetGame()
-    
+
   }
-  else if(cells[playerPosition].classList.contains('home')){ //if frog position has the class home
+  else if (cells[playerPosition].classList.contains('home')) { //if frog position has the class home
     points += 1000 //add 1000 points 
-     //reset frog to starting position 
+    //reset frog to starting position 
     cells[playerPosition].classList.remove('frog')//remove frog
     cells[playerPosition].classList.add('frogReachedHome')//update cell with winning frog 
     playerPosition = 94
@@ -173,7 +173,7 @@ function win (){
   displayScore.innerHTML = `Score ${points}` // updates the innerHTML to current points
 }
 function lose() {
-  if(time === 0 || lives === 0){
+  if (time === 0 || lives === 0) {
     // cells[playerPosition].classList.remove('frog')//remove frog
     // playerPosition = 94
     // cells[playerPosition].classList.add('frog')
@@ -181,7 +181,7 @@ function lose() {
     resetGame()
   }
   // else if(cells[playerPosition].classList.contains('river')){
-  else if(cells[playerPosition].className === 'cell river frog'){
+  else if (cells[playerPosition].className === 'cell river frog') {
     lives--
     displayLives(lives)
     cells[playerPosition].classList.remove('frog')//remove frog
@@ -189,7 +189,7 @@ function lose() {
     cells[playerPosition].classList.add('frog')
     console.log('just died and lives are', lives)
   }
-  else if(cells[playerPosition].classList.contains('carR') || cells[playerPosition].classList.contains('carL')){
+  else if (cells[playerPosition].classList.contains('carR') || cells[playerPosition].classList.contains('carL')) {
     lives--
     displayLives(lives)
     cells[playerPosition].classList.remove('frog')//remove frog
@@ -204,17 +204,17 @@ logsRight.forEach(log => {
 
 function moveLogRight() {
   logsRight.forEach((log, i) => {
-    if(log == 44 || log === 26){ //if the log hits the boundary, then reset
+    if (log == 44 || log === 26) { //if the log hits the boundary, then reset
       cells[log].classList.remove('logR')
       logsRight[i] -= width - 1//update location
       cells[log -= 8].classList.add('logR')
     }
-    else{
+    else {
       cells[log].classList.remove('logR')//remove logR class
       logsRight[i] += 1//update location
       cells[log += 1].classList.add('logR')
     }
-    })
+  })
 
 }
 logsLeft.forEach(log => {
@@ -223,15 +223,15 @@ logsLeft.forEach(log => {
 
 function moveLogLeft() {
   logsLeft.forEach((log, i) => {
-    if(log === 27){ //boundary logic
+    if (log === 27) { //boundary logic
       cells[log].classList.remove('logL')
       logsLeft[i] += (width - 1)//update to starting point location
       cells[log += (width - 1)].classList.add('logL')
     }
-    else{ //move the logs
-    cells[log].classList.remove('logL')
-    logsLeft[i] -= 1//update location
-    cells[log -=1].classList.add('logL')
+    else { //move the logs
+      cells[log].classList.remove('logL')
+      logsLeft[i] -= 1//update location
+      cells[log -= 1].classList.add('logL')
     }
   })
 }
@@ -241,15 +241,15 @@ carsRight.forEach(car => {
 })
 function moveCarRight() {
   carsRight.forEach((car, i) => {
-  if(car === 80){ //if the car hits the boundary, then reset
-    cells[car].classList.remove('carR')
-    carsRight[i] -= (width - 1)//update location
-    cells[car -= (width -1)].classList.add('carR')
-  }else{
-    cells[car].classList.remove('carR')//remove carR class
-    carsRight[i] += 1//update location
-    cells[car +=1].classList.add('carR')//adds carR class
-  }
+    if (car === 80) { //if the car hits the boundary, then reset
+      cells[car].classList.remove('carR')
+      carsRight[i] -= (width - 1)//update location
+      cells[car -= (width - 1)].classList.add('carR')
+    } else {
+      cells[car].classList.remove('carR')//remove carR class
+      carsRight[i] += 1//update location
+      cells[car += 1].classList.add('carR')//adds carR class
+    }
   })
 }
 
@@ -258,35 +258,35 @@ carsLeft.forEach(car => {
 })
 function moveCarLeft() {
   carsLeft.forEach((car, i) => {
-    if(car === 54){ //boundary logic
+    if (car === 54) { //boundary logic
       cells[car].classList.remove('carL')
       carsLeft[i] += (width - 1)//update to starting point location
       cells[car += (width - 1)].classList.add('carL')
     }
-    else{ //move the cars 
-    cells[car].classList.remove('carL')
-    carsLeft[i] -= 1//update location
-    cells[car -=1].classList.add('carL')
+    else { //move the cars 
+      cells[car].classList.remove('carL')
+      carsLeft[i] -= 1//update location
+      cells[car -= 1].classList.add('carL')
     }
   })
 }
 
-function frogOnLog (){// --is there a way of importing that log logic here? log l vs log r
-    if(cells[playerPosition].classList.contains('logR') && !(playerPosition % width === width - 1)){
-      //move frog right
-      cells[playerPosition].classList.remove('frog')//remove logR class
-      playerPosition += 1//update location looky here
-      cells[playerPosition].classList.add('frog')
-      //boundary log if frog hits sides too -> could do this in lose 
-      }
-      else if(cells[playerPosition].classList.contains('logL') && !(playerPosition % width === 0)){
-        cells[playerPosition].classList.remove('frog')
-        playerPosition -= 1
-        cells[playerPosition].classList.add('frog')
-      }
+function frogOnLog() {// --is there a way of importing that log logic here? log l vs log r
+  if (cells[playerPosition].classList.contains('logR') && !(playerPosition % width === width - 1)) {
+    //move frog right
+    cells[playerPosition].classList.remove('frog')//remove logR class
+    playerPosition += 1//update location looky here
+    cells[playerPosition].classList.add('frog')
+    //boundary log if frog hits sides too -> could do this in lose 
+  }
+  else if (cells[playerPosition].classList.contains('logL') && !(playerPosition % width === 0)) {
+    cells[playerPosition].classList.remove('frog')
+    playerPosition -= 1
+    cells[playerPosition].classList.add('frog')
+  }
 }
 
-function ruleCheck(){
+function ruleCheck() {
   const intervalIDRS = setInterval(() => {
     win()
     lose()
